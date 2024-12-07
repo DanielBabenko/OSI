a886b1adfbadc0caa7be0fcf5dc9620b50167188
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2024-12-07 19:50:23 +0300

    Update io-thpt-read.cpp

--- lab1/io-thpt-read.cpp
+++ lab1/io-thpt-read.cpp
@@ -35,23 +35,53 @@ int main(int argc, char* argv[]) {
     cout << "Reading from file: " << filename << endl;
 
     auto start = chrono::high_resolution_clock::now();
-    for (int i = 0; i < repetitions; ++i) {
-        ifstream in(filename, ios::binary);
-        if (!in.is_open()) {
-            cerr << "Error opening file: " << filename << endl;
-            return 1;
+//    for (int i = 0; i < repetitions; ++i) {
+//        ifstream in(filename, ios::binary);
+//        if (!in.is_open()) {
+//            cerr << "Error opening file: " << filename << endl;
+//            return 1;
+//        }
+//        char buffer[block_size];
+//        in.seekg(0, ios::beg);
+//        while (in.read(buffer, block_size)) {
+//            cout << "Read: ";
+//            for (int j = 0; j < 16 && j < block_size; ++j) {
+//                cout << buffer[j];
+//            }
+//            cout << endl;
+//        }
+//        in.close();
+//    }
+
+        HMODULE hKernel32 = GetModuleHandle("Kernel32.dll");
+    
+        if (!hKernel32) {
+            printf("Error: Failed to get handle for Kernel32.dll\n");
+            return 0;
         }
-        char buffer[block_size];
-        in.seekg(0, ios::beg);
-        while (in.read(buffer, block_size)) {
-            cout << "Read: ";
-            for (int j = 0; j < 16 && j < block_size; ++j) {
-                cout << buffer[j];
+        
+        for (int i = 0; i < repetitions; ++i) {
+            HANDLE hFile = CreateFileA(filename.c_str(), GENERIC_READ, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+            if (hFile == INVALID_HANDLE_VALUE) {
+                cerr << "Error opening file: " << GetLastError() << endl;
+                return 1;
             }
-            cout << endl;
+            
+            char buffer[block_size];
+            DWORD bytesRead;
+            while (ReadFile(hFile, buffer, block_size, &bytesRead, nullptr)) {
+                if (bytesRead == 0) break;
+                cout << "Read: ";
+                for (int j = 0; j < bytesRead; ++j) {
+                    cout << buffer[j];
+                }
+                cout << endl;
+
+            }
+
+            CloseHandle(hFile);
         }
-        in.close();
-    }
+
     auto end = chrono::high_resolution_clock::now();
     auto duration = measure_time(start, end);
 
