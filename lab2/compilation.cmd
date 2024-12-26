4040c8fb73a83f8a9615af47615f282fa56ffb4d
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2024-12-26 19:30:52 +0300

    Extra string for test2

--- lab2/compilation.cmd
+++ lab2/compilation.cmd
@@ -1,3 +1,4 @@
 g++ -c -Wall -Wextra -fPIC cache_lib.cpp -o cache_lib.o
 g++ -shared -o cache_lib.dll cache_lib.o -Wl,--out-implib,libcache_lib.dll.a
 g++ test.cpp -o main.exe -L. -lcache_lib
+g++ test2.cpp -o main2.exe -L. -lcache_lib
