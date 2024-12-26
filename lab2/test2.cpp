3e6c4d7418d167be90a0709bd98b934d773b7644
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2024-12-26 18:20:09 +0300

    Test for lab2 functions

---
+++ lab2/test2.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "cache_lib.h"
+
+using namespace std;
+
+#define BLOCK_SIZE 512
+
+int main() {
+    // 1. Инициализация кэша
+    const char* disk_file = "disk.bin";
+    const size_t cache_capacity = 3;
+    std::ofstream disk_stream(disk_file, std::ios::binary);
+    if (disk_stream.is_open()) {
+        char buffer[BLOCK_SIZE];
+        for (int i = 0; i < 10; i++) {
+            sprintf_s(buffer, BLOCK_SIZE, "Data for block %d", i);
+            disk_stream.write(buffer, BLOCK_SIZE);
+        }
+        disk_stream.close();
+    }
+
+
+    if (!cache_init(cache_capacity, disk_file)) {
+        cerr << "Error initializing cache." << endl;
+        return 1;
+    }
+    
+    const char* disk2_file = "disk2.bin";
+    const char* disk3_file = "disk3.bin";
+    // 2. Тестирование lab2_open
+    int fd = lab2_open(disk2_file);
+    if (fd == -1) {
+        cerr << "Error opening file." << endl;
+         cache_destroy();
+        return 1;
+    }
+    cout << "File opened, fd: " << fd << endl;
+    
+    fd = lab2_open(disk3_file);
+    if (fd == -1) {
+        cerr << "Error opening file." << endl;
+         cache_destroy();
+        return 1;
+    }
+    cout << "File opened, fd: " << fd << endl;
+    
+    fd = lab2_open(disk3_file);
+    if (fd == -1) {
+        cerr << "Error opening file." << endl;
+         cache_destroy();
+        return 1;
+    }
+    cout << "File opened, fd: " << fd << endl;
+
+
+    // 3. Тестирование lab2_write
+//    char write_buffer[] = "Hello, cached world!";
+//    ssize_t bytes_written = lab2_write(fd, write_buffer, strlen(write_buffer) );
+//     if (bytes_written == -1) {
+//       std::cerr << "Error writing to file." << std::endl;
+//          lab2_close(fd);
+//            cache_destroy();
+//         return 1;
+//     }
+//     std::cout << "Bytes written: " << bytes_written << std::endl;
+    // 4. Тестирование lab2_read
+//    char read_buffer[100];
+//    ssize_t bytes_read = lab2_read(fd, read_buffer, strlen(write_buffer) );
+//      if (bytes_read == -1) {
+//         std::cerr << "Error reading from file." << std::endl;
+//          lab2_close(fd);
+//            cache_destroy();
+//        return 1;
+//    }
+//      std::cout << "Bytes read: " << bytes_read << std::endl;
+//    
+//      std::cout << "Data read: " << read_buffer << std::endl;
+      
+
+    // 5. Тестирование lab2_lseek
+//    off_t new_offset = lab2_lseek(fd, 5, 0);
+//    if (new_offset == -1) {
+//        std::cerr << "Error seeking file." << std::endl;
+//    } else {
+//        std::cout << "New offset: " << new_offset << std::endl;
+//    }
+
+     // 6. Тестирование lab2_read после lab2_lseek
+//    bytes_read = lab2_read(fd, read_buffer, strlen(write_buffer) - 5 );
+//    if (bytes_read == -1) {
+//        std::cerr << "Error reading from file after lseek." << std::endl;
+//         lab2_close(fd);
+//        cache_destroy();
+//          return 1;
+//    }
+//        std::cout << "Bytes read after seek: " << bytes_read << std::endl;
+//        std::cout << "Data read after seek: " << read_buffer << std::endl;
+
+  // 7. Тестирование lab2_fsync
+//    if (lab2_fsync(fd) == -1) {
+//        std::cerr << "Error synchronizing file." << std::endl;
+//        lab2_close(fd);
+//        cache_destroy();
+//          return 1;
+//    } else {
+//         std::cout << "File synchronized." << std::endl;
+//    }
+
+    // 8. Тестирование lab2_close
+    int f2 = lab2_close(fd);
+    if (f2 == -1) {
+        cerr << "Error closing file." << endl;
+        cache_destroy();
+        return 1;
+    } else {
+         cout << "File " << f2 << " closed." << endl;
+    }
+
+    // 9. Очистка кэша
+      cache_destroy();
+      cout << "Cache destroyed." << endl;
+
+    return 0;
+}
