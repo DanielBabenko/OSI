c98d08fd44c6403dc7cf491befdad7f792355069
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2024-12-26 19:30:06 +0300

    + 4 functions out of 6

--- lab2/cache_lib.h
+++ lab2/cache_lib.h
@@ -11,6 +11,10 @@ __declspec(dllexport) bool cache_init(size_t capacity, const char* disk_file);
 __declspec(dllexport) bool cache_read(int block_id, void* buffer);
 __declspec(dllexport) bool cache_write(int block_id, const void* buffer);
 __declspec(dllexport) void cache_destroy();
+__declspec(dllexport) int lab2_open(const char* path);
+__declspec(dllexport) int lab2_close(int fd);
+__declspec(dllexport) ssize_t lab2_read(int fd, void* buf, size_t count);
+__declspec(dllexport) ssize_t lab2_write(int fd, const void* buf, size_t count);
 
 #ifdef __cplusplus
 }
