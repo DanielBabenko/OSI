df03444f734da139493c7d532b6eb2c3fbe8f6a5
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2025-01-08 21:26:19 +0300

    Update compilation.cmd

--- lab2/compilation.cmd
+++ lab2/compilation.cmd
@@ -2,3 +2,4 @@ g++ -c -Wall -Wextra -fPIC cache_lib.cpp -o cache_lib.o
 g++ -shared -o cache_lib.dll cache_lib.o -Wl,--out-implib,libcache_lib.dll.a
 g++ test.cpp -o main.exe -L. -lcache_lib
 g++ test2.cpp -o main2.exe -L. -lcache_lib
+g++ io-thpt-read.cpp -o benchmark2.exe -L. -lcache_lib
