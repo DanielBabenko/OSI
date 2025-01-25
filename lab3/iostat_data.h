635ab1166c01fde6c4f1f3c33edbf0695928b6d5
DanielBabenko <119009518+DanielBabenko@users.noreply.github.com>
2025-01-25 21:48:09 +0300

    Update iostat_data.h

--- lab3/iostat_data.h
+++ lab3/iostat_data.h
@@ -1,14 +1,13 @@
 #ifndef IOSTAT_DATA_H
 #define IOSTAT_DATA_H
 
-#define BDEVNAME_SIZE 256
-
+// Структура для передачи данных пользователю
 struct iostat_data {
-    unsigned long long read_sectors;
-    unsigned long long write_sectors;
-    unsigned long long read_ios;
-    unsigned long long write_ios;
-    char path[BDEVNAME_SIZE];
+    pid_t pid;
+    unsigned long long bytes_read;
+    unsigned long long bytes_write;
+    unsigned long long read_time_ns;
+    unsigned long long write_time_ns;
 };
 
 #endif
